0bb99cd0e9636a89e863f5fa74c260790abab361
Yaseen Ayatullah Khan <24216496@siswa.um.edu.my>
2025-12-22 15:43:32 +0800

    Fixed app.py

--- src/app.py
+++ src/app.py
@@ -40,7 +40,7 @@ st.markdown(bg_style + """
 
     /* REMOVE THE GAP BELOW THE TITLE */
     h1 {
-        margin-top: -50px !important;
+        margin-top: 50px !important;
         margin-bottom: 0px !important;
         padding-bottom: 0px !important;
     }
